d40fa8d4fb7c0a8b2d957647662929d483f76e5a
Mikey Robinson <mikeyrobinson@mmc.school.nz>
2020-06-26 11:44:39 +1200

    Icons Drarwn On Panel

---
+++ Dodge Task/FrmDodge.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodge_Task
+{
+    public partial class FrmDodge : Form
+    {
+        Graphics g; //declare a graphics object called g
+                    // declare space for an array of 7 objects called planet 
+        Planet[] planet = new Planet[7];
+        Random yspeed = new Random();
+        Spaceship spaceship = new Spaceship();
+
+
+        public FrmDodge()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                int x = 10 + (i * 75);
+                planet[i] = new Planet(x);
+
+            }
+
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            //call the Planet class's DrawPlanet method to draw the image planet1 
+            for (int i = 0; i < 7; i++)
+            {
+                // generate a random number from 5 to 20 and put it in rndmspeed
+                int rndmspeed = yspeed.Next(5, 20);
+                planet[i].y += rndmspeed;
+
+                //call the Planet class's drawPlanet method to draw the images
+                planet[i].DrawPlanet(g);
+                spaceship.DrawSpaceship(g);
+            }
+
+
+        }
+
+        private void TmrPlanet_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                planet[i].MovePlanet();
+                //if a planet reaches the bottom of the Game Area reposition it at the top
+                if (planet[i].y >= PnlGame.Height)
+                {
+                    planet[i].y += 5;
+                }
+
+
+            }
+            PnlGame.Invalidate();//makes the paint event fire to redraw the panel
+        }
+    }
+}
---
+++ Dodge Task/Planet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Dodge_Task
+{
+    class Planet
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet(int spacing)
+        {
+            x = spacing;
+
+            y = 10;
+            width = 20;
+            height = 20;
+            //planetImage contains the plane1.png image
+            planetImage = Properties.Resources.planet1;
+            planetRec = new Rectangle(x, y, width, height);
+        }
+        // Methods for the Planet class
+        public void DrawPlanet(Graphics g)
+        {
+            
+            g.DrawImage(planetImage, planetRec);
+            planetRec = new Rectangle(x, y, width, height);
+        }
+        public void MovePlanet()
+        {
+            y += 5;
+
+            planetRec.Location = new Point(x, y);
+        }
+
+    }
+}
