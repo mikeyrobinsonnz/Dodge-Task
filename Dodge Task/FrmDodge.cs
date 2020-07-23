975ee95b94fbea04171e813ae229ca2b52c0d4a5
Mikey Robinson <mikeyrobinson@mmc.school.nz>
2020-07-24 11:47:15 +1200

    game finished.

--- Dodge Task/FrmDodge.cs
+++ Dodge Task/FrmDodge.cs
@@ -150,7 +150,9 @@ namespace Dodge_Task
 
         private void FrmDodge_Load(object sender, EventArgs e)
         {
-            lives = int.Parse(txtLives.Text);// pass lives entered from textbox to lives variable
+            MessageBox.Show("Use the left and right arrow keys to move the spaceship. \n Don't get hit by the planets! \n Every planet that gets past scores a point. \n If a planet hits a spaceship a life is lost! \n \n Enter your Name press tab and enter the number of lives \n Click Start to begin", "Game Instructions");
+            TxtName.Focus();
+
         }
     }
 }
